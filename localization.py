00beec32a07c21d4bd05200cc4e41044229005df
Chrisforza <71526171+Chrisforza@users.noreply.github.com>
2024-11-11 11:39:21 -0500

    First Lab 3 Work Session

--- localization.py
+++ localization.py
@@ -47,19 +47,19 @@ class localization(Node):
         
         # TODO Part 3: Set up the quantities for the EKF (hint: you will need the functions for the states and measurements)
         
-        x= ...
+        x= np.array([0, 0, 0, 0, 0, 0])
         
-        Q= ...
+        Q= 0.5*np.identity(6, float)
 
-        R= ...
+        R= 0.5*np.identity(6, float)
         
-        P= ... # initial covariance
+        P= np.identity(6, float) # initial covariance # SHOULD THIS BE JUST IDENTITY MATRIX???
         
         self.kf=kalman_filter(P,Q,R, x, dt)
         
         # TODO Part 3: Use the odometry and IMU data for the EKF
-        self.odom_sub=message_filters.Subscriber(...)
-        self.imu_sub=message_filters.Subscriber(...)
+        self.odom_sub=message_filters.Subscriber(self, odom, "/odom", qos_profile=odom_qos)
+        self.imu_sub=message_filters.Subscriber(self, Imu, "/imu", qos_profile=odom_qos)
         
         time_syncher=message_filters.ApproximateTimeSynchronizer([self.odom_sub, self.imu_sub], queue_size=10, slop=0.1)
         time_syncher.registerCallback(self.fusion_callback)
@@ -71,7 +71,7 @@ class localization(Node):
         # your measurements are the linear velocity and angular velocity from odom msg
         # and linear acceleration in x and y from the imu msg
         # the kalman filter should do a proper integration to provide x,y and filter ax,ay
-        z=...
+        z= np.array([odom_msg.twist.twist.linear.x, odom_msg.twist.twist.angular.z, imu_msg.linear_acceleration.x, imu_msg.linear_acceleration.y])
         
         # Implement the two steps for estimation
         ...
@@ -80,10 +80,10 @@ class localization(Node):
         xhat=self.kf.get_states()
 
         # Update the pose estimate to be returned by getPose
-        self.pose=np.array(...)
+        self.pose=np.array(xhat[0], xhat[1], xhat[2], imu_msg.header.stamp)
 
         # TODO Part 4: log your data
-        self.loc_logger.log_values(...)
+        self.loc_logger.log_values(imu_msg.linear_acceleration.x, imu_msg.linear_acceleration.y)
       
     def odom_callback(self, pose_msg):
         
